de912b654ce6de200c311c2a70256a7400f72464
Vazgen0404 <vazgen_avetisyan0404@mail.ru>
2022-01-26 20:25:28 +0400

    Added Employees

---
+++ Zoo/Cages/Cage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    abstract class Cage
+    {
+        private static int id;
+        public int Id { get; set; }
+        public IEnumerable<Animal> Animals { get; set; }
+        public Plate Plate { get; set; }
+
+        public Cage(IEnumerable<Animal> animals)
+        {
+            id++;
+            Id = id;
+            Animals = animals;
+            Plate = new Plate();
+
+        }
+    }
+}
---
+++ Zoo/Cages/Plate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    class Plate
+    {
+        public List<Food> Foods { get; set; }
+        public Plate()
+        {
+            Foods = new List<Food>();
+        }
+        public void AddFood(Food food)
+        {
+            Foods.Add(food);
+        }
+    }
+}
