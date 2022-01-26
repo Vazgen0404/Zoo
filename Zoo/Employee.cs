4bfa4a65c96a8067dcab8c64727b78aa90fb5e40
Vazgen0404 <vazgen_avetisyan0404@mail.ru>
2022-01-26 20:13:18 +0400

    Added Cages

---
+++ Zoo/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    abstract class Employee
+    {
+        private int id;
+        public int Id { get; set; }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { if (!String.IsNullOrEmpty(value)) name = value; }
+        }
+
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 18 && value < 60)
+                {
+                    age = value;
+                }
+                else throw new Exception("The age of the employee does not meet the requirements");
+            }
+        }
+
+        public List<Cage> CustomCages { get; set; }
+        public Employee(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            CustomCages = new List<Cage>();
+        }
+
+        public void AddCustomCage(Cage cage)
+        {
+            CustomCages.Add(cage);
+        }
+
+        public abstract void Function();
+    }
+}
---
+++ Zoo/FeedingEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    class FeedingEmployee : Employee
+    {
+        public FeedingEmployee(string name, int age) : base(name,age)
+        {
+
+        }
+
+        public override void Function()
+        {
+            FeedAnimals();
+        }
+
+        private void FeedAnimals()
+        {
+            
+        }
+    }
+}
